27b7fa183793f923ff36dba9a292e86fb7289195
cagansit <cagan.sit@beyn.com.tr>
2019-11-05 21:59:49 +0300

    initializing project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cagans42/memosocket
+
+go 1.13
+
+require github.com/spf13/viper v1.5.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import viper "github.com/spf13/viper"
+
+func init() {
+
+}
+
+func main() {
+
+}
